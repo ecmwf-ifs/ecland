3a7ceaa68c9212538d43e7ae7fa8f2eacddf52e7
Iria Ayan-Miguez <iria.ayan-miguez_2@ecmwf.int>
2023-02-23 15:50:15 +0000

    An improvement when creates the CaMa-Flood communicator

--- src/surf/cmflood/cmf_ctrl_maps_mod.F90
+++ src/surf/cmflood/cmf_ctrl_maps_mod.F90
@@ -9,6 +9,9 @@ MODULE CMF_CTRL_MAPS_MOD
 !
 ! (C) D.Yamazaki & E. Dutra  (U-Tokyo/FCUL)  Aug 2019
 !
+! Modifications:
+! -- I. Ayan-Miguez (BSC) Feb 2023: Added reading of mpireg file in netcdf format
+!
 ! Licensed under the Apache License, Version 2.0 (the "License");
 !   You may not use this file except in compliance with the License.
 !   You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
