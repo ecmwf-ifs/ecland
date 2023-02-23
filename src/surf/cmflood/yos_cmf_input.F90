f6f5bb988948d8a87bec4f47a5e1655e4892c3f1
Iria Ayan-Miguez <iria.ayan-miguez_2@ecmwf.int>
2023-02-23 15:50:15 +0000

    An improvement when creates the CaMa-Flood communicator

--- src/surf/cmflood/yos_cmf_input.F90
+++ src/surf/cmflood/yos_cmf_input.F90
@@ -3,6 +3,9 @@ MODULE YOS_CMF_INPUT
 !* PURPOSE: Shared variables for CaMa-Flood model configulation
 ! (C) D.Yamazaki & E. Dutra  (U-Tokyo/FCUL)  Aug 2019
 !
+! Modifications:
+! I.Ayan-Miguez (BSC), Feb 2023: Added Fluxes buffers for IFS coupling  
+!
 ! Licensed under the Apache License, Version 2.0 (the "License");
 !   You may not use this file except in compliance with the License.
 !   You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
