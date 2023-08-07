f3d4b2f3d78e4aa6346bb541e4bef843cb580726
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2023-08-07 13:20:03 +0000

    GPD and associated pointers now memory blocked

--- src/surf/offline/driver/cnt41s.F90
+++ src/surf/offline/driver/cnt41s.F90
@@ -27,6 +27,7 @@ USE MPL_MODULE
 USE YOMLOG1S , ONLY: NDIMCDF
 USE YOMDPHY  , ONLY: NLALO ,NLAT     ,NLON, NPOI, NPOIP
 USE YOMGPD1S , ONLY: VFPGLOB, VFCLAKE, VFCLAKEF,  VFITM
+USE YOMDIM1S , ONLY: NPROMA
 USE OMP_LIB
 USE MPL_MODULE
 #ifdef DOC
@@ -86,6 +87,8 @@ REAL (KIND=JPRB),ALLOCATABLE :: ZD1STSRO2(:), ZD1STRO2(:), ZD1STIEVAPU2(:), ZVFP
 REAL (KIND=JPRD) :: ZFDPD
 REAL(KIND=JPHOOK)  :: ZHOOK_HANDLE
 INTEGER(KIND=JPIM) :: MYPROC, NPROC
+INTEGER(KIND=JPIM) :: IST,IEND,IBL,IPROMA,IL
+REAL(KIND=JPRB), ALLOCATABLE :: SEND_BUF(:)
 
 #include "dattim.intfb.h"
 #include "updtim1s.intfb.h"
@@ -108,6 +111,7 @@ ALLOCATE(ZD1STRO2(NLALO))
 ALLOCATE(ZD1STIEVAPU2(NLALO))
 ALLOCATE(ZVFPGLOB(NLALO))
 ALLOCATE(DCMFCOM(NLALO,1))
+ALLOCATE(SEND_BUF(NPOI))
 ZBUFFO(:,:,:)=0._JPRB
 ZBUFFI(:,:,:)=0._JPRB
 ZFLD(:,:)=0._JPRB
@@ -119,7 +123,8 @@ DCMFCOM(:,:)=0._JPRB
 
 
 IF (LECMF1WAY) THEN
-  CALL MPL_GATHERV(PRECVBUF=ZVFPGLOB(:),KROOT=1,PSENDBUF=VFPGLOB(:),KRECVCOUNTS=NPOIP(:),CDSTRING="CNT41S:gaussianIndex")
+  CALL PACK_BUFFER(VFPGLOB, SEND_BUF)
+  CALL MPL_GATHERV(PRECVBUF=ZVFPGLOB(:),KROOT=1,PSENDBUF=SEND_BUF,KRECVCOUNTS=NPOIP(:),CDSTRING="CNT41S:gaussianIndex")
   IF( MYPROC == 1 ) THEN
   ! Sanity checks
     IF (NDIMCDF == 2)THEN
@@ -265,23 +270,37 @@ DO NSTEP=NSTART,NSTOP
       ENDIF
       IF (LECMF2LAKEC==0) THEN
         ! no coupling 
-        VFCLAKEF(:) = VFCLAKE(:)
+        VFCLAKEF(:,:) = VFCLAKE(:,:)
       ELSEIF (LECMF2LAKEC==1) THEN 
         ! replace lake cover by flood plain fraction over land 
-        DO JL=1,NLALO
-          IF ( VFITM(JL) > 0.5_JPRB ) THEN
-            ! Update lake fraction only over land points
-            VFCLAKEF(JL) = MAX(0._JPRB,MIN(0.99_JPRB,DCMFCOM(JL,1)))
-          ENDIF
+        !$OMP PARALLEL DO PRIVATE(IST,IEND,IBL)
+        DO IST = 1, NLALO, NPROMA
+          IEND = MIN(IST+NPROMA-1,NLALO)
+          IBL = (IST-1)/NPROMA + 1
+          DO JL=IST,IEND
+            IL = JL-IST+1
+            IF ( VFITM(IL,IBL) > 0.5_JPRB ) THEN
+              ! Update lake fraction only over land points
+              VFCLAKEF(IL,IBL) = MAX(0._JPRB,MIN(0.99_JPRB,DCMFCOM(JL,1)))
+            ENDIF
+          ENDDO
         ENDDO
+        !$OMP END PARALLEL DO
       ELSEIF (LECMF2LAKEC==2) THEN 
         ! add flooplain fraction to lake cover over land 
-        DO JL=1,NLALO
-          IF ( VFITM(JL) > 0.5_JPRB ) THEN
-            ! Update lake fraction only over land points
-            VFCLAKEF(JL) = MAX(0._JPRB,MIN(0.99_JPRB,VFCLAKE(JL)+DCMFCOM(JL,1)))
-          ENDIF
+        !$OMP PARALLEL DO PRIVATE(IST,IEND,IBL)
+        DO IST = 1, NLALO, NPROMA
+          IEND = MIN(IST+NPROMA-1,NLALO)
+          IBL = (IST-1)/NPROMA + 1
+          DO JL=IST,IEND
+            IL = JL-IST+1
+            IF ( VFITM(IL,IBL) > 0.5_JPRB ) THEN
+              ! Update lake fraction only over land points
+              VFCLAKEF(IL,IBL) = MAX(0._JPRB,MIN(0.99_JPRB,VFCLAKE(IL,IBL)+DCMFCOM(JL,1)))
+            ENDIF
+          ENDDO
         ENDDO
+        !$OMP END PARALLEL DO
       ELSE
         WRITE(NULOUT,*) "LECMF2LAKEC can be only 0,1 or 2 but it is",LECMF2LAKEC
         CALL ABOR1('LECMF2LAKEC can only be 0,1 or 2')
@@ -325,11 +344,28 @@ ENDIF
 !              -------------------------------------
 
 CLOSE(NULFOR)
+DEALLOCATE(SEND_BUF)
 
 !     ------------------------------------------------------------------
 
 IF (LHOOK) CALL DR_HOOK('CNT41S',1,ZHOOK_HANDLE)
 
-RETURN
+CONTAINS
+  SUBROUTINE PACK_BUFFER(ARR, SEND_BUF)
+    IMPLICIT NONE
+
+    REAL(KIND=JPRB), INTENT(IN) :: ARR(:,:)
+    REAL(KIND=JPRB), INTENT(INOUT) :: SEND_BUF(:)
+
+    !$OMP PARALLEL DO PRIVATE(IST,IEND,IBL,IPROMA)
+    DO IST = 1, NPOI, NPROMA
+      IEND = MIN(IST+NPROMA-1,NPOI)
+      IBL = (IST-1)/NPROMA + 1
+      IPROMA = IEND-IST+1
+
+      SEND_BUF(IST:IEND) = ARR(1:IPROMA,IBL)
+    ENDDO
+    !$OMP END PARALLEL DO
+  END SUBROUTINE PACK_BUFFER
 
 END SUBROUTINE CNT41S
