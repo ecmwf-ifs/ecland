f33baaaf470a9e92aaea260ef14e427fa43935a7
Gabriele Arduini <gabriele.arduini@ecmwf.int>
2024-01-04 14:23:17 +0000

    use average t2m also over dominant water/ice consistently as for all other tiles
    fixes for tl/ad code
    
    cleaning

--- src/surf/module/surfpps_ctl_mod.F90
+++ src/surf/module/surfpps_ctl_mod.F90
@@ -293,16 +293,6 @@ IF (LDPPCFLS) THEN
                          &PFRTI(JL,JTILE)*ZQ2M(JL,JTILE)
       ENDDO
     ENDDO
-    ! Overwrite where dominant water or sea-ice. 
-    ! Using PLSM would be cleaner, but this should be eq.
-    DO JL=KIDIA,KFDIA
-    IF ((PFRTI(JL,3)+PFRTI(JL,4)+PFRTI(JL,5)+PFRTI(JL,6)+PFRTI(JL,7)+PFRTI(JL,8)+PFRTI(JL,9))<1.0_JPRB)THEN
-        PT2M(JL)=ZT2M_DL(JL)
-        PD2M(JL)=ZD2M_DL(JL)
-        PQ2M(JL)=ZQ2M_DL(JL)
-      ENDIF
-    ENDDO
-
  
   ELSE ! Business as usual
     ZZ0MW(KIDIA:KFDIA)=MAX(PZ0MW(KIDIA:KFDIA),PZ0HW(KIDIA:KFDIA))
