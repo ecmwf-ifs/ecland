26ea0eb39bbcedbfab887cc041a032d6141fb729
Vincent Huijnen <Vincent.Huijnen@knmi.nl>
2024-08-20 10:03:11 +0000

    test excluding KTILE==3

--- src/surf/module/vsurf_mod.F90
+++ src/surf/module/vsurf_mod.F90
@@ -828,7 +828,8 @@ ENDIF !tiles 4, 6, 7 , 8, 10
 
 IF ( LEMIS_BVOC ) THEN   
   PBVOCFLUX(KIDIA:KFDIA,:)=0._JPRB
-  IF (KTILE==3 .OR. KTILE==4 .OR. KTILE==6 .OR. KTILE==7 ) THEN
+  !VH  IF (KTILE==3 .OR. KTILE==4 .OR. KTILE==6 .OR. KTILE==7 ) THEN
+  IF (KTILE==4 .OR. KTILE==6 .OR. KTILE==7 ) THEN
 
 
 !   Set the soil temperature to be used for acclimation of photosynthetic traits
