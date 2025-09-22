f5ab9808bfef1293590b80c0db53d8ecf8f839b3
Zbigniew Piotrowski <zbigniew.piotrowski@ecmwf.int>
2025-09-22 12:09:02 +0200

    Adding missing license text to newly added files

--- src/surf/module/susdp_dflt_ctl_mod.F90
+++ src/surf/module/susdp_dflt_ctl_mod.F90
@@ -9,6 +9,14 @@ USE YOS_SOIL, ONLY : TSOIL
 USE YOS_AGS , ONLY : TAGS
 USE YOMSURF_SSDP_MOD
 
+! (C) Copyright 2025- ECMWF.
+!
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+
 !     ---------------------------------------------------------------------------
 !**   *SUSDP_DFLT* - INITIALIZE THE SDP PARAMETERS WITH DEFAULT VALUES
 
