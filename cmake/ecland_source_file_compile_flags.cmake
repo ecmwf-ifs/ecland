34769032b5df1f6b7fbed0a90f367c7241d65e42
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2025-11-18 19:08:56 +0000

    NVHPC: disable openmp for CPG1S in debug builds

--- cmake/ecland_source_file_compile_flags.cmake
+++ cmake/ecland_source_file_compile_flags.cmake
@@ -9,6 +9,15 @@ if (CMAKE_Fortran_COMPILER_ID MATCHES "PGI|NVHPC")
                 module/surfexcdriverstl_ctl_mod.F90
             PROPERTIES COMPILE_OPTIONS "-g;-O1"
         )
+    else()
+        # TODO: we need this because compilation with nvhpc hangs interminably when
+        # debugging symbls are requested for cpg1s.F90, so we disable it for debug builds.
+        # This may be fixed with the introduction of the memory blocking, so this should
+        # be revisited then.
+        set_source_files_properties(
+                offline/driver/cpg1s.F90
+            PROPERTIES COMPILE_OPTIONS "-Mnoopenmp"
+        )
     endif()
 
 endif()
